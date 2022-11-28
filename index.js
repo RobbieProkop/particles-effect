31d20b9565d33363d248e563cd57a239505aea4c
Robbie Prokop <robbieprokop@gmail.com>
2022-11-28 19:16:17 +0400

    create draw and update mthods

--- index.js
+++ index.js
@@ -13,7 +13,39 @@ let mouse = {
   radius: (canvas.height / 80) * (canvas.width / 80),
 };
 
+//set x & y position to mouse position
 window.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
 });
+
+//create particles
+class Particle {
+  constructor(x, y, directionX, directionY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+  }
+
+  //method to draw individual particle
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#8c5523";
+    ctx.fill();
+  }
+
+  // check particle position, check mouse position, move the particle, draw the particle
+  update() {
+    //check if particle is still within canvas
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX;
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY;
+    }
+  }
+}
